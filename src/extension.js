68afc8a11f61a36dfa23db91c7cb08f274d3102b
Rafael Franco <rafaelfr@outlook.com>
2018-05-27 21:17:41 -0300

    Updated extension

--- src/extension.js
+++ src/extension.js
@@ -1,15 +1,22 @@
-function toast(message, type) {
-  let container = document.querySelector('.toast-container')
+function toast({ message, type, duration, position }) {
+  let container = document.querySelector('.notices')
   if (!container) {
-    container = document.createElement("div")
-    container.className = "toast-container"
+    container = document.createElement('div')
+    container.className = 'notices'
     document.body.appendChild(container)
   }
 
-  let toast = document.createElement("div")
-  toast.classList = `notification ${type}`
+  let toast = document.createElement('div')
+  let classes = ['notification']
+  if(type) classes.push(type)
+  if(position) classes.push(position)
+  if(type)
+  toast.classList = classes.join(' ')
   toast.innerText = message
 
   container.appendChild(toast)
-  console.log(message)
+
+  setTimeout(() => {
+    toast.remove()
+  }, duration)
 }
