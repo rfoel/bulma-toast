5db066545b7891066a2a028e3072f648d7e9ef8f
Rafael Franco <rafaelfr@outlook.com>
2018-05-29 12:03:45 -0300

    Update

--- src/extension.js
+++ src/extension.js
@@ -1,37 +1,35 @@
-export class BulmaToast {
-  open({ message, type, duration, position, dismissible }) {
-    let noticesTop = document.querySelector('.notices.is-top')
-    let noticesBottom = document.querySelector('.notices.is-bottom')
+export default function toast({ message, type, duration, position, dismissible }) {
+  let noticesTop = document.querySelector('.notices.is-top')
+  let noticesBottom = document.querySelector('.notices.is-bottom')
 
-    if (!noticesTop || !noticesBottom) {
-      noticesTop = document.createElement('div')
-      noticesBottom = document.createElement('div')
-      noticesTop.className = 'notices is-top'
-      noticesBottom.className = 'notices is-bottom'
-      document.body.appendChild(noticesTop)
-      document.body.appendChild(noticesBottom)
-    }
-
-    let toast = document.createElement('div')
-    let classes = ['notification']
-    if (type) classes.push(type)
-    if (position) classes.push(position)
-    toast.classList = classes.join(' ')
-    if (dismissible) {
-      let dismissButton = document.createElement('button')
-      dismissButton.className = 'delete'
-      dismissButton.addEventListener('click', () => {
-        toast.remove()
-      })
-      toast.insertAdjacentElement('afterbegin', dismissButton)
-    }
-    toast.insertAdjacentText('beforeend', message)
-
-    if (position.includes('is-bottom')) noticesBottom.appendChild(toast)
-    else noticesTop.appendChild(toast)
-
-    setTimeout(() => {
-      toast.remove()
-    }, duration)
+  if (!noticesTop || !noticesBottom) {
+    noticesTop = document.createElement('div')
+    noticesBottom = document.createElement('div')
+    noticesTop.className = 'notices is-top'
+    noticesBottom.className = 'notices is-bottom'
+    document.body.appendChild(noticesTop)
+    document.body.appendChild(noticesBottom)
   }
+
+  let toast = document.createElement('div')
+  let classes = ['notification']
+  if (type) classes.push(type)
+  if (position) classes.push(position)
+  toast.classList = classes.join(' ')
+  if (dismissible) {
+    let dismissButton = document.createElement('button')
+    dismissButton.className = 'delete'
+    dismissButton.addEventListener('click', () => {
+      toast.remove()
+    })
+    toast.insertAdjacentElement('afterbegin', dismissButton)
+  }
+  toast.insertAdjacentText('beforeend', message)
+
+  if (position.includes('is-bottom')) noticesBottom.appendChild(toast)
+  else noticesTop.appendChild(toast)
+
+  setTimeout(() => {
+    toast.remove()
+  }, duration)
 }
