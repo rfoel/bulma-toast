1b47d06ba9e58f9417ac6f805efb6b1830ebc75c
Rafael Franco <rafaelfr@outlook.com>
2019-02-11 10:00:23 -0200

    Fix typo

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 const { toast } = require("../dist/bulma-toast.cjs");
 
-test("toast should be a funcion", () => {
+test("toast should be a function", () => {
   expect(typeof toast).toBe("function");
 });
