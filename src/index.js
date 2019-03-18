2d5516a1e2744ef949d2ef5bb4020f8a8c8bfcd4
DaZhou <45910169+dazhoudotnet@users.noreply.github.com>
2019-03-19 00:54:18 +0800

    fix 4 ie 11.
     if you add or remove multiple classes at once, it won't work in IE11, Firefox 24- and Chrome 24-.

--- src/index.js
+++ src/index.js
@@ -112,7 +112,8 @@ class Toast {
       classes.push(`animated ${this.animate.in}`);
       this.onAnimationEnd(() => this.element.classList.remove(this.animate.in));
     }
-    this.element.classList = classes.join(" ");
+    // this.element.classList = classes.join(" ");
+    this.element.className = classes.join(" ");
     if (this.dismissible) {
       let dismissButton = doc.createElement("button");
       dismissButton.className = "delete";
