e38c1c30639e50858d6cb860f1c22c2eb87bef8b
Rafael Franco <rafaelfr@outlook.com>
2019-10-08 23:44:15 -0300

    Add better testing

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,31 @@
-const { toast } = require("../dist/bulma-toast.cjs");
+const { toast, setDoc } = require("../src");
 
-test("toast should be a function", () => {
-  expect(typeof toast).toBe("function");
+describe("toast", () => {
+  beforeEach(() => {
+    document.body.innerHTML = "<button class='toast'>Toast</button>";
+    document.querySelector(".toast").addEventListener("click", () =>
+      toast({
+        message: "Hello there",
+        type: "is-primary",
+        position: "top-left",
+        dismissible: true,
+        duration: 1000,
+        pauseOnHover: true,
+        animate: { in: "fadeIn", out: "fadeOut" }
+      })
+    );
+  });
+
+  it("should not contain a notification", () => {
+    const notification = document.querySelector(".notification");
+    expect(notification).toBeFalsy();
+  });
+
+  it("should contain a notification", () => {
+    const button = document.querySelector(".toast");
+    button.click();
+    const notification = document.querySelector(".notification");
+    expect(notification.textContent).toBe("Hello there");
+    expect(notification.classList.contains("is-primary")).toBeTruthy();
+  });
 });
