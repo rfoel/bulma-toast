d29154067eb3a591ee3792a40cdc16dd4cc9ce3d
Rafael <rafaelfr@outlook.com>
2020-04-14 10:16:49 -0300

    chore: update package structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,33 @@
-import minify from "rollup-plugin-babel-minify";
-import babel from 'rollup-plugin-babel';
-import pkg from "./package.json";
+import minify from 'rollup-plugin-babel-minify'
+import babel from 'rollup-plugin-babel'
+import pkg from './package.json'
+
+const plugins = [
+  babel({
+    exclude: 'node_modules/**',
+  }),
+  minify({
+    banner: `/*!\n * ${pkg.name} ${pkg.version} \n * (c) 2018-present ${pkg.author} \n * Released under the ${pkg.license} License.\n */`,
+    bannerNewLine: true,
+  }),
+]
 
 export default [
   {
-    input: "src/index.js",
+    input: 'src/index.js',
     output: {
-      name: "bulmaToast",
+      name: 'bulmaToast',
       file: pkg.browser,
-      format: "umd"
+      format: 'umd',
     },
-    plugins: [
-      babel({
-        exclude: 'node_modules/**'
-      }),
-      minify({
-        banner: `/*!\n * ${pkg.name} ${pkg.version} \n * (c) 2018-present ${
-          pkg.author
-          } \n * Released under the ${pkg.license} License.\n */`,
-        bannerNewLine: true
-      })
-    ]
+    plugins,
   },
   {
-    input: "src/index.js",
+    input: 'src/index.js',
     output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" }
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
     ],
-    plugins: [
-      babel({
-        exclude: 'node_modules/**'
-      }),
-      minify({
-        banner: `/*!\n * ${pkg.name} ${pkg.version} \n * (c) 2018-present ${
-          pkg.author
-          } \n * Released under the ${pkg.license} License.\n */`,
-        bannerNewLine: true
-      })
-    ]
-  }
-];
+    plugins,
+  },
+]
