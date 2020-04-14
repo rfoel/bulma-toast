fe14db92b8ee4076e1c61373d83770c2e0b9dcc5
Rafael <rafaelfr@outlook.com>
2020-04-14 10:16:49 -0300

    chore: update package structure

---
+++ src/index.test.js
@@ -0,0 +1,43 @@
+import { toast, setDoc } from '.'
+
+describe('toast', () => {
+  beforeEach(() => {
+    document.body.innerHTML = "<button class='toast'>Toast</button>"
+    document.querySelector('.toast').addEventListener('click', () =>
+      toast({
+        message: 'Hello there',
+        type: 'is-primary',
+        position: 'top-left',
+        dismissible: true,
+        duration: 1000,
+        pauseOnHover: true,
+        single: true,
+      }),
+    )
+
+    setDoc(document) // cleaning up
+  })
+
+  it('should not contain a notification', () => {
+    const notification = document.querySelector('.notification')
+    expect(notification).toBeFalsy()
+  })
+
+  it('should contain a notification', () => {
+    const button = document.querySelector('.toast')
+    button.click()
+    const notification = document.querySelector('.notification')
+    expect(notification.textContent).toBe('Hello there')
+    expect(notification.classList.contains('is-primary')).toBeTruthy()
+  })
+
+  it('should remove notification with container on close', () => {
+    const button = document.querySelector('.toast')
+    button.click()
+    const notification = document.querySelector('.notification')
+    expect(notification.textContent).toBe('Hello there')
+    expect(notification.classList.contains('is-primary')).toBeTruthy()
+    notification.querySelector('.delete').click()
+    expect(document.body.querySelector('div')).toBeFalsy()
+  })
+})
